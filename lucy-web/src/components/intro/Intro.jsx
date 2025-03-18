139fe8f5215690832d79457db91749aeef710b7a
Barbora6 <smetanovabarbora@email.cz>
2025-03-18 08:17:16 +0100

    uprava textu + druhé tlacitko

--- lucy-web/src/components/intro/Intro.jsx
+++ lucy-web/src/components/intro/Intro.jsx
@@ -1,20 +1,23 @@
 import "./Intro.css";
 import photoLucka from "../../images/lucka-photo.jpg";
+import btnImg from "../../assets/hireme.png";
+import { Link } from "react-scroll";
 
 export const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
-        <span className="hello">Hello,</span>
+        <span className="hello">Hello, </span>
         <span className="introText">
-          I´m <span className="introName">Lucy</span>, <br /> English teacher
+          I´m <span className="introName">Lucy</span>, <br /> The English
+          teacher
         </span>
-        <p className="box introPara1">
+        {/* <p className="box introPara1">
           Mým hlavním zaměřením je angličtina, kterou jsem vystudovala na
           Masarykově univerzitě v Brně. Kromě angličny hovořím francouzky. Kromě
           studia jsem oba jazyky pilovala i během zahraničních pobytů. výukou
           jazyku na ZŠ Strání. Od roku 2023 učím na volné noze.
-        </p>
+        </p> */}
         <p className="box introPara2">
           Chcete zlepšit svoji angličtinu nebo němčinu? Procvičit si konverzaci
           a zbavit se českého přízvuku? Rozšířit slovní zásobu a vyznat se v
@@ -27,7 +30,14 @@ export const Intro = () => {
           Výuku přizpůsobím vašim potřebám a stylu učení, abyste dosáhli co
           nejlepších výsledků.
         </p>
+        <Link>
+          <button className="btn">
+            <img src={btnImg} alt="" className="btnImg" />
+            Hire me
+          </button>
+        </Link>
       </div>
+
       <img src={photoLucka} alt="Profile" className="bg-photo" />
     </section>
   );
